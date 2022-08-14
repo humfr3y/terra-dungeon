8a273e367310fa01d4749f440aa33289154394a4
Humfrey <47324267+humfr3y@users.noreply.github.com>
2022-08-14 14:25:22 +0300

    Update SupporterScene.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/SupporterScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/SupporterScene.java
@@ -5,6 +5,9 @@
  * Shattered Pixel Dungeon
  * Copyright (C) 2014-2022 Evan Debenham
  *
+ * Terra Dungeon
+ * Copyright (C) 2022 Dmitriy Malenko
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
